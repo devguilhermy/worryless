8e6ec1daf6a7289b10438a5fe353880ffed2d729
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2020-11-28 12:31:19 -0300

    Database connection using env variables

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@maincluster.l3ttc.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@maincluster.l3ttc.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 export default mongoose;
